202fac7332fd4a9c539b9ec5f40aa31867bf7e60
vgouttin7 <victor.gouttin@ucalgary.ca>
2024-02-15 16:41:33 -0700

    Medical Record done

--- edu/ucalgary/oop/MedicalRecord.java
+++ edu/ucalgary/oop/MedicalRecord.java
@@ -1,5 +1,56 @@
 package edu.ucalgary.oop;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MedicalRecord {
-    
+    private Location location;
+    private String treatmentDetails;
+    private String dateOfTreatment;
+    private static final String REGEX = "\\d{4}-\\d{2}-\\d{2}";
+    private static final Pattern datePattern = Pattern.compile(REGEX);
+
+    public MedicalRecord(Location location, String treatmentDetails, String dateOfTreatment) throws IllegalArgumentException {
+        Matcher match = datePattern.matcher(dateOfTreatment);
+        if(match.matches()){
+            this.dateOfTreatment = dateOfTreatment;
+            this.location = location;
+            this.treatmentDetails = treatmentDetails;
+        }
+        
+        else {
+            throw new IllegalArgumentException("This format is wrong, please use YYYY-MM-DD format.");
+        }
+
+    }
+
+    public Location getLocation() {
+        return this.location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public String getTreatmentDetails() {
+        return this.treatmentDetails;
+    }
+
+    public void setTreatmentDetails(String treatmentDetails) {
+        this.treatmentDetails = treatmentDetails;
+    }
+
+    public String getDateOfTreatment() {
+        return this.dateOfTreatment;
+    }
+
+    public void setDateOfTreatment(String dateOfTreatment) throws IllegalArgumentException{
+        Matcher match = datePattern.matcher(dateOfTreatment);
+        if(match.matches()){
+            this.dateOfTreatment = dateOfTreatment;
+        }
+        else {
+            throw new IllegalArgumentException("This format is wrong, please use YYYY-MM-DD format.");
+        }
+    }
 }
