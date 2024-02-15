4f75d7ce37b84697b1e7225a65bd8a94eeb22ea9
boumster <95254000+boumster@users.noreply.github.com>
2024-02-15 11:25:16 -0700

    initial files

---
+++ edu/ucalgary/oop/DisasterVictim.java
@@ -0,0 +1,10 @@
+package edu.ucalgary.oop;
+
+public class DisasterVictim {
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String comments;
+    private int ASSIGNED_SOCIAL_ID;
+    private MedicalRecord [] medicalRecords;
+}
